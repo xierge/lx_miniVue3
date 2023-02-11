aba960bee5d5a9c872425622f8b4de734991371f
lipengxi <2899952565@qq.com>
2023-02-11 23:47:51 +0800

    ✨ feat:xxx

--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -1,7 +1,17 @@
-(function () {
-	'use strict';
+/*
+ * @Date: 2023-02-11 22:11:00
+ * @LastEditors: lipengxi 2899952565@qq.com
+ * @LastEditTime: 2023-02-11 23:47:30
+ * @FilePath: /lx_miniVue3/packages/vue/dist/vue.js
+ * @description:
+ */
+;(function () {
+  'use strict'
 
-	console.log('lx');
+  var isString = function (str) {
+    return typeof str === 'string'
+  }
 
-})();
+  console.log(isString('lx'))
+})()
 //# sourceMappingURL=vue.js.map
